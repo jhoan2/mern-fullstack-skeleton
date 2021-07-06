d19055fe296d0ddabe4d0bb08c8c8c99391c0535
John Hoang <67968051+jhoan2@users.noreply.github.com>
2021-07-06 13:43:34 -0700

    edit profile component

--- client/MainRouter.js
+++ client/MainRouter.js
@@ -4,6 +4,9 @@ import Home from "./core/Home";
 import Users from "./user/Users";
 import Signup from "./user/Signup";
 import Signin from "./auth/Signin";
+import Profile from "./user/Profile";
+import EditProfile from "./user/EditProfile";
+
 const MainRouter = () => {
   return (
     <div>
@@ -12,6 +15,8 @@ const MainRouter = () => {
         <Route path="/users" component={Users} />
         <Route path="/signup" component={Signup} />
         <Route path="/signin" component={Signin} />
+        <PrivateRoute path="/user/edit/:userId" component={EditProfile} />
+        <Route path="/user/:userId" component={Profile} />
       </Switch>
     </div>
   );
